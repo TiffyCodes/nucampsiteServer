78dfe6fbc67dd1251921ed04b5739be039730146
Tiffany W <t.washington0008@gmail.com>
2023-01-22 13:45:04 -0800

    Express REST API with MongoDB and Mongoose Part 2.

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -119,6 +119,182 @@ campsiteRouter.route("/:campsiteId")
 });
 //*********
 
+//******* To access comments (which is a sub) inside of the campsites document */
+campsiteRouter.route('/:campsiteId/comments')
 
-//****** */
+.get((req, res, next) => {
+    //res and headers are set by app.all so only out...
+    Campsite.findById(req.params.campsiteId)
+    //now we need to access only the comments for this SINGULARcampsite, not all campsite info
+    .then(campsite => {
+        if (campsite) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        //will send campsite.comments
+        res.json(campsite.comments);
+        //the above will automatically close the stream afterwards, so don't need res.end
+    } else {
+        err = new Error(`Campsite ${req.params.campsiteId} not found.`);
+        err.status = 404;
+        return next(err);
+    }
+    })
+    // res.end('Will send all the campsites to you');
+    .catch(err => next(err));
+})
+//post req for the campsites path, once it hits next at all, it will go to the next relevant method
+.post((req, res, next) => {
+    //will get the info from the request body, mongoose will check the data to make sure it fits the Schema
+    Campsite.findById(req.params.campsiteId)
+    //now we need to access only the comments for this SINGULARcampsite, not all campsite info
+    .then(campsite => {
+        if (campsite) {
+        campsite.comments.push(req.body);
+        //to save the comment.. lower c campsite NOT uppercase
+        campsite.save()
+        .then(campsite => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            //will send campsite.comments
+            res.json(campsite.comments);
+        //the above will automatically close the stream afterwards, so don't need res.end
+        })
+        .catch(err => next(err));
+    } else {
+        err = new Error(`Campsite ${req.params.campsiteId} not found.`);
+        err.status = 404;
+        return next(err);
+    }
+    })
+    .catch(err => next(err));
+})
+//don't need to add next here bc put is not allowed
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}/comments`);
+})
+.delete((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+    //now we need to access only the comments for this SINGULARcampsite, not all campsite info
+    .then(campsite => {
+        if (campsite) {
+        //delete every one of the comments array-- using a for loop to iterate for e/ comment since want to delete all
+        for (let i = (campsite.comments.length-1); i >=0; i--) {
+            campsite.comments.id(campsite.comments[i]._id).remove();
+        }
+        //to save the comment.. lower c campsite NOT uppercase
+        campsite.save()
+        .then(campsite => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            //will send campsite.comments
+            res.json(campsite.comments);
+        //the above will automatically close the stream afterwards, so don't need res.end
+        })
+        .catch(err => next(err));
+    } else {
+        err = new Error(`Campsite ${req.params.campsiteId} not found.`);
+        err.status = 404;
+        return next(err);
+    }
+    })
+    .catch(err => next(err));
+});
+
+
+//******  FOR SINGLE COMMENT DELETE*/
+campsiteRouter.route('/:campsiteId/comments/:commentId')
+
+.get((req, res, next) => {
+    //res and headers are set by app.all so only out...
+    Campsite.findById(req.params.campsiteId)
+    //now we need to access only the comments for this SINGULARcampsite, not all campsite info
+    .then(campsite => {
+        if (campsite && campsite.comments.id(req.params.commentId)) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        //will send campsite.comments
+        res.json(campsite.comments.id(req.params.commentId));
+        //the above will automatically close the stream afterwards, so don't need res.end
+    } else if (!campsite){
+        err = new Error(`Campsite ${req.params.campsiteId} not found.`);
+        err.status = 404;
+        return next(err);
+    }   else {
+        err = new Error(`Comment ${req.params.commentId} not found.`);
+        err.status = 404;
+        return next(err);
+    }
+    })
+    // res.end('Will send all the campsites to you');
+    .catch(err => next(err));
+})
+//post req for the campsites path, once it hits next at all, it will go to the next relevant method
+.post((req, res, next) => {
+    //not supported
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`);
+})
+//don't need to add next here bc put is not allowed
+.put((req, res, next) => {
+    //we'd only want to update certain parts of the comment- not author or title
+    Campsite.findById(req.params.campsiteId)
+    //now we need to access only the comments for this SINGULARcampsite, not all campsite info
+    .then(campsite => {
+        if (campsite && campsite.comments.id(req.params.commentId)) {
+            if (req.body.rating) {
+                campsite.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                campsite.comments.id(req.params.commentId).text = req.body.text;
+            }
+            campsite.save()
+            .then(campsite => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(campsite);
+            })
+            .catch(err => next(err));
+        //the above will automatically close the stream afterwards, so don't need res.end
+    } else if (!campsite){
+        err = new Error(`Campsite ${req.params.campsiteId} not found.`);
+        err.status = 404;
+        return next(err);
+    }   else {
+        err = new Error(`Comment ${req.params.commentId} not found.`);
+        err.status = 404;
+        return next(err);
+    }
+    })
+    // res.end('Will send all the campsites to you');
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+    //now we need to access only the comments for this SINGULARcampsite, not all campsite info
+    .then(campsite => {
+        if (campsite && campsite.comments.id(req.params.commentId)) {
+            campsite.comments.id(req.params.commentId).remove();
+            campsite.save()
+            .then(campsite => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(campsite);
+            })
+            .catch(err => next(err));
+        //the above will automatically close the stream afterwards, so don't need res.end
+    } else if (!campsite){
+        err = new Error(`Campsite ${req.params.campsiteId} not found.`);
+        err.status = 404;
+        return next(err);
+    }   else {
+        err = new Error(`Comment ${req.params.commentId} not found.`);
+        err.status = 404;
+        return next(err);
+    }
+    })
+    .catch(err => next(err));
+});
+
+
 module.exports = campsiteRouter;
